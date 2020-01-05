5d5d668f3964206fee78dc13e0be4afca29def83
wuxiaoyu <fox-fly@163.com>
2020-01-05 23:27:49 +0800

    rename file

--- async.py
+++ async.py
@@ -1,13 +1,17 @@
 import asyncio
 import time
+import pytest
 
 async def download_url(url):
     print('start get %s' % url)
     await asyncio.sleep(2)
     print('get %s finished.' % url)
 
-if __name__ == '__main__':
+def test_download_url(url):
     start_time = time.time()
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(download_url('https:www.baidu.com')) # 阻塞等事件完成之后再向下运行。相当于进程线程的join()方法，或者进线程池的wait()
+    loop.run_until_complete(download_url(url)) # 阻塞等事件完成之后再向下运行。相当于进程线程的join()方法，或者进线程池的wait()
     print('一共用时：%s' % (time.time() - start_time))
+
+if __name__ == '__main__':
+    test_download_url('https:www.baidu.com')
