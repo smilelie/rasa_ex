cf8d58280afaf991b49831733b8f40a7279d1326
wuxiaoyu <fox-fly@163.com>
2020-01-05 23:08:18 +0800

    update actions

--- actions.py
+++ actions.py
@@ -1,29 +1,51 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
-from typing import Any, Text, Dict, List
-
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-import logging
-
-class ActionHelloWorld(Action):
-
+class ActionSearchConcerts(Action):
     def name(self):
-        return "action_hello_world"
+        return "action_search_concerts"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(self, dispatcher, tracker, domain):
+        concerts = [
+            {"artist": "Foo Fighters", "reviews": 4.5},
+            {"artist": "Katy Perry", "reviews": 5.0},
+        ]
+        description = ", ".join([c["artist"] for c in concerts])
+        dispatcher.utter_message(f"{description}")
+        return [SlotSet("concerts", concerts)]
 
-        dispatcher.utter_message(text="Hello World!")
 
-        logging.warning("ActionHelloWorld.run......")
+class ActionSearchVenues(Action):
+    def name(self):
+        return "action_search_venues"
 
+    def run(self, dispatcher, tracker, domain):
+        venues = [
+            {"name": "Big Arena", "reviews": 4.5},
+            {"name": "Rock Cellar", "reviews": 5.0},
+        ]
+        dispatcher.utter_message("here are some venues I found")
+        description = ", ".join([c["name"] for c in venues])
+        dispatcher.utter_message(f"{description}")
+        return [SlotSet("venues", venues)]
+
+
+class ActionShowConcertReviews(Action):
+    def name(self):
+        return "action_show_concert_reviews"
+
+    def run(self, dispatcher, tracker, domain):
+        concerts = tracker.get_slot("concerts")
+        dispatcher.utter_message(f"concerts from slots: {concerts}")
+        return []
+
+
+class ActionShowVenueReviews(Action):
+    def name(self):
+        return "action_show_venue_reviews"
+
+    def run(self, dispatcher, tracker, domain):
+        venues = tracker.get_slot("venues")
+        dispatcher.utter_message(f"venues from slots: {venues}")
         return []
